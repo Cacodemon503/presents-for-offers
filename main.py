ef15225cdd4eea8a917a5066fb9d533ca2583887
Sergey Bruk <42474487+Cacodemon503@users.noreply.github.com>
2020-08-07 16:23:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,35 @@ import random
 import base64
 import json
 from urllib.parse import parse_qs
+import smtplib
+from email.mime.text import MIMEText
 
+def email_notification(name, cashback):
+    from_who = 'XXX@gmail.com'
+    to = 'XXX@'
+    if cashback != 'Код не действителен!':
+        msg = MIMEText(f'Code: {name} \nPresent: {cashback}')
+        msg['Subject'] = 'New valid action'
+    else:
+        msg = MIMEText(f'Code: {name} \nПОПЫТКА НЕСАНКЦИОНИРОВАННОГО ДОСТУПА')
+        msg['Subject'] = 'Unauthorized action'
+    msg['From'] = from_who
+    msg['To'] = to
+
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.starttls()
+    smtpObj.login('XXX@gmail.com', 'XXX')
+    smtpObj.sendmail(from_who, to, msg.as_string())
+    smtpObj.quit()
 
 def offer_cashback(name):
-    if name == '':  # insert valid promocode
+    
+    if name[0].lower() == 'w' and name[1:].lower() in {'', ''}:  # insert valid promocodes
         lst = ['Подписка на "Okko"',
                'Подписка на "IVI"',
                'Подарочная карта "AMEDIATEKA"',
                'Подароный сертификат "Wink"',
                'Сертификат на покупку "Lamoda"',
-               'Сертификат на покупку украшений "Moments"',
-               'Сертификат на покупку украшений "Leski Store"',
-               'Сертификат на покупку "Вeauty365"',
                'Сертификат на покупку "Golden Apple"',
                'Подарочный сертификат "Яндекс.Плюс"',
                'Сертификат на покупку "Ozon"',
@@ -27,7 +44,36 @@ def offer_cashback(name):
                'Подарочный сертификат "Combo от Mail.ru"',
                'Подарочный сертификат "WILDBERRIES"',
                'Подарочный сертификат "Яндекс.Еда"',
-               'Подарочная карта "Клуб привилегий"']
+               'Подарочная карта "Клуб привилегий"',
+               'Подарочный сертификат "Яндекс.Музыка"',
+               'Подарочная карта Яндекс.Такси',
+               'Подарочная карта "Ситимобил"',
+               'Подарочная карта "Литрес"',
+               'Сертификат в "Спортмастер"']
+
+        luck = str(random.choice(lst))
+        cashback = 'Твой приз: {}!'.format(luck)
+        return cashback
+    elif name[0].lower() == 'm' and name[1:].lower() in {'', ''}: # insert valid promocodes
+        lst = ['Подписка на "Okko"',
+               'Подписка на "IVI"',
+               'Подарочная карта "AMEDIATEKA"',
+               'Подароный сертификат "Wink"',
+               'Сертификат на покупку "Lamoda"',
+               'Подарочный сертификат "Яндекс.Плюс"',
+               'Сертификат на покупку "Ozon"',
+               'Сертификат на покупку "Лабиринт"',
+               'Сертификат на покупку "Республика"',
+               'Подарочная карта "Storytel"',
+               'Подарочный сертификат "Combo от Mail.ru"',
+               'Подарочный сертификат "Яндекс.Еда"',
+               'Подарочная карта "Клуб привилегий"',
+               'Подарочный сертификат "Яндекс.Музыка"',
+               'Подарочная карта Яндекс.Такси',
+               'Подарочная карта "Ситимобил"',
+               'Подарочная карта "Литрес"',
+               'Сертификат в "Спортмастер"',
+               'Сертификат на выбор в Steam, PS Store или Xbox']
 
         luck = str(random.choice(lst))
         cashback = 'Твой приз: {}!'.format(luck)
@@ -70,13 +116,15 @@ def handler(event, context):
     name = name.strip()
 
     cashback = offer_cashback(name)
+    email_notification(name, cashback)
+
     return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-        'isBase64Encoded': False,
-        'body': json.dumps({
-            "message": cashback
-        }, ensure_ascii=False)
-    }
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'isBase64Encoded': False,
+            'body': json.dumps({
+                "message": cashback
+            }, ensure_ascii=False)
+        }
